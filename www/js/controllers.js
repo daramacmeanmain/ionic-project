a4a28b528fa5b754276f637e803ec37ab6dd9a31
daramacmeanmain <dara.macmeanmain@outlook.ie>
2016-04-24 15:52:52 +0100

    Added files via upload

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,9 +2,20 @@ angular.module('starter.controllers', ['ionic'])
 
 .controller('appCtrl', function($scope, $ionicModal) 
 {
-    $scope.currTasks = [];
-    $scope.compTasks = [];
+    $scope.currTasks = []; //scope for new tasks
+    $scope.checkedTasks = [ //scope for checking tasks
+        {
+            checkedItem: "", checked: false
+            
+        }
+            ];
+   /* if(checkedItem.checked === true)
+        $scope.compTasks = function(c)
+            {
+                $scope.compTasks.push({ taskItem: c.newTask + ' - ' + c.newDate})
+            };*/
     
+    //Opening modal window
     $ionicModal.fromTemplateUrl('templates/tabs.html', 
     {
         scope: $scope
@@ -13,15 +24,12 @@ angular.module('starter.controllers', ['ionic'])
     {
         $scope.modal = modal;
     });
+        //Taking in task from the user
         $scope.createTask = function(t) 
         {   
             $scope.currTasks.push({ taskItem: t.newTask + ' - ' + t.newDate});
             $scope.modal.hide();
         }
-
-    /*$scope.submit = function() {
-        if ($scope.text) {
-          $scope.list.push(this.text);
-          $scope.text = '';
-    }};*/
+        
+    
 })
